142a98f9714eff6a11966b5db92f9cb74a460ae0
shaqc <shaq.cruz2012@gmail.com>
2023-01-28 23:39:36 -0500

    updated app.js and todolist.html

--- todolist/app.js
+++ todolist/app.js
@@ -14,11 +14,93 @@ If you complete this to-do-list see if you can have your to-do's persist by plac
 **Avoid copying any of the to-do-list tutorials online, remember you need to escape tutorial hell if you hope to be a great dev**
 
 */
+// Create an empty array to store the to-do items
+let todos = []
 
+// check if there are any to-do items in local storage, if so set todos array to that value
+if (localStorage.getItem("todos")){
+  todos = JSON.parse(localStorage.getItem("todos"))
+}
+
+// get references to the form and task list elements
+const form = document.querySelector("form")
+const tasklist = document.querySelector(".task-list")
+
+// handle the form submit event
+form.addEventListener("submit", function(e) {
+  e.preventDefault()
+
+  // get task input value
+  const taskInput = document.querySelector("#task-input")
+  const task = taskInput.value 
+
+  // add the task to the todos array
+  todos.push({task, completed: false})
+
+  //save the todos array to local storage
+  localStorage.setItem("todos", JSON.stringify(todos))
+  //clear the task input value
+  taskInput.value = ""
+  //render the todo list
+  render()
+  }
+)
+//render the to-do list
+function render() {
+  //clear existing task list
+  tasklist.innerHTML = ""
+  //iterate over the todos array
+  todos.forEach(function(todo, index){
+    //create a new list item
+    const taskItem = document.createElement("li")
+    taskItem.classList.add("list-group-item")
+    //add the task text
+    taskItem.innerHTML = todo.task
+    //add the "completed" class if the task is completed
+    if (todo.completed){
+      taskItem.classList.add("completed")
+    }
+    // create the "complete" button
+    const completeBtn = document.createElement("button")
+    completeBtn.classList.add("btn","btn-success","complete-btn")
+    completeBtn.innerHTML = "Complete"
+    completeBtn.addEventListener("click",function() {
+      //mark task as completed
+      todos[index].completed = true
+      //save todos array to local storage
+      localStorage.setItem("todos", JSON.stringify(todos))
+      //render the todo list
+      render()
+    })
+
+    //create the delete button
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("btn", "btn-danger", "delete-btn")
+    deleteBtn.innerHTML = "Delete"
+    deleteBtn.addEventListener("click", function(){
+      //remove the task for the todos array
+      todos.splice(index,1)
+      //save the todos array to local storage
+      localStorage.setItem("todos", JSON.stringify(todos))
+      //render todo list
+      render()
+    })
+    //append buttons to the list item
+    taskItem.appendChild(completeBtn)
+    taskItem.appendChild(deleteBtn)
+    //append the list item to the task list
+    tasklist.appendChild(taskItem)
+  })
+}
+// render initial todo list
+render()
+
+
+
+/* Old Code Below
 //newtask, push, tasklist
 
-/*Set the initial voluMe of count in local storage when
-page loads*/
+//Set the initial voluMe of count in local storage whenpage loads
 localStorage.setItem(count, input)
 const getStr = localStorage.getItem('count')
 console.log(getStr)
@@ -55,3 +137,4 @@ function updateDOM() {
   const countEl = document.querySelector('h4')
   countEl.textContent = botscore
 }
+*/
